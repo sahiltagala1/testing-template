bfef495958b4df6b9f89f42ca5819b00f69a2ace
sahil tagala <sahiltagala1@gmail.com>
2023-03-11 00:15:39 +0530

    update

---
+++ classify-parameters.js
@@ -0,0 +1,57 @@
+const messages = require('./language');
+const lang = messages.en;
+
+function checkRange(val, min, max) {
+  if (val < min) {
+    return {inRange: false, breachType: lang.low};
+  } else if (val > max) {
+    return {inRange: false, breachType: lang.high};
+  } else {
+    return {inRange: true};
+  }
+}
+
+function checkWarning(val, min, max, tolerance) {
+  const upperWarningLimit = max - max * tolerance;
+  const lowerWarningLimit = min + max * tolerance;
+
+  const rangeClassifications = [
+    {
+      range: val <= lowerWarningLimit,
+      warningType: lang.low,
+      isWarning: true,
+    },
+    {
+      range: val >= upperWarningLimit,
+      warningType: lang.high,
+      isWarning: true,
+    },
+  ];
+
+  const matchedClassification = rangeClassifications.find(({range}) => range);
+
+  return matchedClassification ? matchedClassification : {isWarning: false};
+}
+
+function classifyParameters(
+    paramName,
+    paramVal,
+    lowerLimit,
+    upperLimit,
+    tolerance,
+) {
+  const resRange = checkRange(paramVal, lowerLimit, upperLimit);
+  const resWarning = checkWarning(paramVal, lowerLimit, upperLimit, tolerance);
+  if (!resRange.inRange) {
+    console.log(`${paramName} ${lang.breachType(resRange.breachType)}`);
+    return resRange.inRange;
+  } else if (resWarning.isWarning) {
+    console.log(`${paramName} ${lang.warningType(resWarning.warningType)}`);
+    return true;
+  } else {
+    console.log(`${paramName} ${lang.normal}`);
+    return true;
+  }
+}
+
+module.exports = classifyParameters;
